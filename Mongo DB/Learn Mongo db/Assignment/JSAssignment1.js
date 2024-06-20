9670dc4e6d5953ccc33328a20dbd29ca4212d714
faizanpasha356 <mdalifaizan356@gmail.com>
2024-06-20 22:41:25 +0530

    Start Backend

---
+++ Mongo DB/Learn Mongo db/Assignment/JSAssignment1.js
@@ -0,0 +1,25 @@
+/* 
+Filter and Map Questions
+How would you get an array of user emails of all users who are admins?How would you get an array
+of users who have the skill 'Python'?
+How would you get an array of users who are older than 30?How would you get an array of usernames
+of all users who live in 'Goa'?
+How would you get an array of users whose salary is greater than 1,000,000?How would you get an
+array of female users?How would you get an array of first names of all users who are customers?
+How would you get an array of users who have 'JavaScript' as one of their skills?
+How would you get an array of users who live in 'Mumbai' and have a salary less than 800,000?
+How would you get an array of male users who are customers?
+
+Find Questions
+How would you find the first user who lives in 'New Delhi'?
+How would you find the first user who has the skill 'React'?
+How would you find the first user who is an admin and lives in 'Jaipur'?
+How would you find the first user with a salary of exactly 900,000?How would you find the first
+user whose username is 'Rahul Verma'?
+Splice and Slice QuestionsHow would you remove the first user who lives in 'Kolkata' from the array?
+How would you get a new array containing the first 5 users?
+How would you get a new array containing the last 5 users?
+How would you create a new array with users from the 3rd to the 7th position (inclusive)?
+How would you replace the user at the 10th position with a new user object?
+
+*/
