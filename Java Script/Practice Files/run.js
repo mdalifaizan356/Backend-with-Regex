f2d5b471938201c3b9ca2749e9e151c9c02c863c
faizanpasha356 <mdalifaizan356@gmail.com>
2024-06-25 23:23:10 +0530

    Practice

---
+++ Java Script/Practice Files/run.js
@@ -0,0 +1,17 @@
+// User se 4 input lena hai, inka sum nikalna hai. Agar sum > 100 hota hai to ek function call
+// karna hai, jisme 3 input lena hai aur unka average return karna hai. Agar returned average > 50
+// hai to ek aur function call karke factorial nikalna hai aur check karna hai wo prime number hai
+// ya nahi.
+
+
+let num1=5,num2=10,num3=80,num4=30;
+let sum=num1+num2+num3+num4;
+console.log(sum);
+
+if(sum>100){
+    average();
+}
+
+let average=()=>{
+    
+}
