f0d1ac75b47595d0e110e6b1765a5cc0e4082462
faizanpasha356 <mdalifaizan356@gmail.com>
2024-06-26 23:20:14 +0530

    Practice

--- Java Script/Practice Files/run.js
+++ Java Script/Practice Files/run.js
@@ -3,15 +3,48 @@
 // hai to ek aur function call karke factorial nikalna hai aur check karna hai wo prime number hai
 // ya nahi.
 
+// var sum = (() => {
+//     let num1=5, num2=10, num3=80, num4=40;
+//     return num1+num2+num3+num4;
+//   })();                                                   //IIFE
 
-let num1=5,num2=10,num3=80,num4=30;
-let sum=num1+num2+num3+num4;
-console.log(sum);
+//   if(sum>100){
+//     var avgVal = (arg1,arg2,arg3) => {
+//         return arg1+arg2+arg3/3;
+//       };
+//     // console.log(avgVal(5,5,5));
+//     if(avgVal(5,5,5)<50){
+//         const factorial = (avgValue) => {
+//             if (avgValue === 0 || avgValue === 1) {
+//             return 1;
+//             }
+//             return avgValue * factorial(avgValue - 1);
+//         }
+//         console.log(factorial(avgVal));
+//     }
+//   }
+
+
+var sum = (() => {
+  let num1=5, num2=10, num3=80, num4=40;
+  return num1+num2+num3+num4;
+})();                                                   //IIFE
+
+function avg(n1,n2,n3){
+  return n1+n2+n3/3;
+};
+
+const factorial = (avgValue) => {
+              if (avgValue === 0 || avgValue === 1) {
+              return 1;
+              }
+              return avgValue * factorial(avgValue - 1);
+          }
+          console.log(factorial(avgVal));
 
 if(sum>100){
-    average();
-}
-
-let average=()=>{
-    
+  let avgVal=avg(50, 60, 70);
+  if(avgVal>50){
+    let factVal=factorial(avgVal);
+  }
 }
