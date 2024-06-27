07fd4f6d8b0b8afc470e2145250bbdd068328c9a
faizanpasha356 <mdalifaizan356@gmail.com>
2024-06-27 23:34:33 +0530

    Practice

--- Java Script/Practice Files/run.js
+++ Java Script/Practice Files/run.js
@@ -1,50 +1,20 @@
-// User se 4 input lena hai, inka sum nikalna hai. Agar sum > 100 hota hai to ek function call
-// karna hai, jisme 3 input lena hai aur unka average return karna hai. Agar returned average > 50
-// hai to ek aur function call karke factorial nikalna hai aur check karna hai wo prime number hai
-// ya nahi.
+// User se 3 input lena hai, inka sum nikalna hai. Agar sum 0 hota hai to ek function call karna
+// hai aur check karna hai ki wo perfect square hai ya nahi. Agar perfect square hai to uska square
+// root nikalna hai aur result return karna hai.
 
-// var sum = (() => {
-//     let num1=5, num2=10, num3=80, num4=40;
-//     return num1+num2+num3+num4;
-//   })();                                                   //IIFE
+let n1=0, n2=0, n3=0
+let sum =n1+n2+n3;
+console.log(sum);
 
-//   if(sum>100){
-//     var avgVal = (arg1,arg2,arg3) => {
-//         return arg1+arg2+arg3/3;
-//       };
-//     // console.log(avgVal(5,5,5));
-//     if(avgVal(5,5,5)<50){
-//         const factorial = (avgValue) => {
-//             if (avgValue === 0 || avgValue === 1) {
-//             return 1;
-//             }
-//             return avgValue * factorial(avgValue - 1);
-//         }
-//         console.log(factorial(avgVal));
-//     }
-//   }
-
-
-var sum = (() => {
-  let num1=5, num2=10, num3=80, num4=40;
-  return num1+num2+num3+num4;
-})();                                                   //IIFE
-
-function avg(n1,n2,n3){
-  return n1+n2+n3/3;
-};
-
-const factorial = (avgValue) => {
-              if (avgValue === 0 || avgValue === 1) {
-              return 1;
-              }
-              return avgValue * factorial(avgValue - 1);
-          }
-          console.log(factorial(avgVal));
-
-if(sum>100){
-  let avgVal=avg(50, 60, 70);
-  if(avgVal>50){
-    let factVal=factorial(avgVal);
+const perfectSquare=(sum)=>{
+    let sqrt = Math.sqrt(sum);
+    return (sqrt % 1 === 0);
   }
+
+
+if(sum===0){
+  if(perfectSquare(sum=true)){
+    console.log(Math.sqrt(sum));
+
+  };
 }
