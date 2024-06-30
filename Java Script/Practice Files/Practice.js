495154f4bfa07724cea6accc8fd6d4d135751ae4
faizanpasha356 <mdalifaizan356@gmail.com>
2024-06-30 23:58:19 +0530

    Practice

--- Java Script/Practice Files/Practice.js
+++ Java Script/Practice Files/Practice.js
@@ -1,12 +1,8 @@
-// Object
-const person = {
-    name: 'John Doe',
-    age: 30,
-    country: 'USA'
-  };
-  
-  // Object destructuring
-  const { name, age } = person;
-  
-  console.log(name); // Output: 'John Doe'
-  console.log(age);  // Output: 30
+// Return Value: map aur filter dono new arrays return karte hain, jabki find ek single element return karta hai.
+// Functionality: map transforms array ke har element ko, filter specific elements ko select karta hai, aur find pehla matching element ko retrieve karta hai.
+// Use Case: map transformations ke liye use hota hai, filter selection ke liye, aur find ek specific element ko retrieve karne ke liye.
+
+
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled); // Output: [2, 4, 6, 8, 10]
