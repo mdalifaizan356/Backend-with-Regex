dd84c4dc355c4c95e3140fad8d4c8c8c6c02407c
faizanpasha356 <mdalifaizan356@gmail.com>
2024-07-03 23:41:10 +0530

    Work On project

--- Java Script/Practice Files/Practice.js
+++ Java Script/Practice Files/Practice.js
@@ -1,8 +1,25 @@
-// Return Value: map aur filter dono new arrays return karte hain, jabki find ek single element return karta hai.
-// Functionality: map transforms array ke har element ko, filter specific elements ko select karta hai, aur find pehla matching element ko retrieve karta hai.
-// Use Case: map transformations ke liye use hota hai, filter selection ke liye, aur find ek specific element ko retrieve karne ke liye.
+const myarray1=[1,2,3,4,5];
+const myarray2 = new Array([5,4,3,2,1]);
+
+// console.log(myarray1);
+// console.log(myarray2);
 
 
-const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map((num) => num * 2);
-console.log(doubled); // Output: [2, 4, 6, 8, 10]
+// at()
+// console.log(myarray1.at(2));
+// console.log(myarray1[2]);         //Both are give same output
+// console.log(myarray1.at(-1));
+// console.log(myarray1.at());       //By default value is 0.    
+// console.log(myarray1[-1]);     
+
+
+//concate()
+// const myarray3 = myarray1.concat(myarray2);      // It is join two array
+
+
+//join()
+// const myarray3 = myarray1.join("and");          //It is Retrun new array as a string
+
+//slice()
+// const myarray3 = myarray1.slice(0,4);
+// console.log(myarray3);
